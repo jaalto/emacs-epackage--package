19e830c27888a31b917309638010148e4e4f6f5f
Jari Aalto <jari.aalto@cante.net>
2012-01-05 10:31:04 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/elpa-epkg-examples.el
@@ -0,0 +1,13 @@
+;; Prevent loading this file. Study the examples.
+(error "elpa-epkg-examples.el is not a configuration file.")
+
+;; For Emacs 24 version of package.el only. DOES NOT WORK
+;; WITH OLD package.el
+(setq package-archives
+      ;; see http://www.emacswiki.org/emacs/ELPA
+      '(("ELPA" . "http://tromey.com/elpa/")
+	("gnu" . "http://elpa.gnu.org/packages/")
+	;; See http://marmalade-repo.org
+	("marmalade" . "http://marmalade-repo.org/packages/")))
+
+;; End of file
